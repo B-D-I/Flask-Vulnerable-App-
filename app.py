8840e8fedd6b3a0951a75f7c011a12ee83d17a15
Nathan <salomb3@gmail.com>
2022-12-13 22:13:18 +0000

    update

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import Flask, render_template, flash, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import datetime as dt
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from creds import mysql_config
+from creds import mysql_config, csrf_secret_key, client_id, client_secret
 from forms import *
+from authlib.integrations.flask_client import OAuth
+
 
 app = Flask(__name__)
 
@@ -13,7 +15,26 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = mysql_config
 
 # secret key csrf token
-app.config['SECRET_KEY'] = "password"
+app.config['SECRET_KEY'] = csrf_secret_key
+
+# recaptcha
+app.config['RECAPTCHA_PUBLIC_KEY'] = site_key2
+app.config['RECAPTCHA_PRIVATE_KEY'] = secret_key2
+
+
+oauth = OAuth(app)
+oauth.register(
+    name='google',
+    client_id=client_id,
+    client_secret=client_secret,
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
+    client_kwargs={'scope': 'openid email profile'})
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -56,24 +77,41 @@ class Users(db.Model, UserMixin):
 #### ROUTES ####
 
 # LOGIN
-@app.route('/login', methods=['GET', 'POST'])
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         # get the first (unique) username
+#         user = Users.query.filter_by(username=form.username.data).first()
+#         if user:
+#             if check_password_hash(user.password_hash, form.password.data):
+#                 login_user(user)
+#                 flash("Login successful")
+#                 return redirect(url_for('dashboard'))
+#             else:
+#                 flash("Incorrect Credentials")
+#         else:
+#             flash("Incorrect Credentials")
+#
+#     return render_template('login.html', form=form)
+
+
+@app.route('/login')
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        # get the first (unique) username
-        user = Users.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password_hash, form.password.data):
-                login_user(user)
-                flash("Login successful")
-                return redirect(url_for('dashboard'))
-            else:
-                flash("Incorrect Credentials")
-        else:
-            flash("Incorrect Credentials")
+    google = oauth.create_client('google')
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
 
-    return render_template('login.html', form=form)
-
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    # user = oauth.google.userinfo()
+    # session['profile'] = user_info
+    # session.permanent = True
+    return redirect('/')
 
 # LOGOUT
 @app.route('/logout', methods=['GET', 'POST'])
@@ -193,7 +231,6 @@ def name():
 @app.route('/')
 def index():
     return render_template("index.html")
-
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,20 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, Form, ValidationError
 from wtforms.validators import DataRequired, EqualTo, Length
+from flask_wtf.recaptcha import RecaptchaField
+from creds import site_key2, secret_key2
+import safe
+
+
+class CaptchaForm(Form):
+    captcha = RecaptchaField(public_key=site_key2, private_key=secret_key2, secure=True)
 
 
 class UserForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired(), Length(max=25, message='name too long')])
     email = StringField("Email", validators=[DataRequired()])
-    username = StringField("Username", validators=[DataRequired()])
-    role = StringField("Role")
-    password_hash = PasswordField("Password", validators=[DataRequired(), Length(min=9, message='password too short'),
-                                                          EqualTo('password_hash2', message='passwords must match')])
+    username = StringField("Username", validators=[DataRequired(), Length(max=25, message='username too long')])
+    role = StringField("Role", validators=[Length(max=25, message='role type too long')])
+    password_hash = PasswordField("Password", validators=[DataRequired(),
+                                                          Length(min=9, max=35, message='password must be greater than'
+                                                                 '9 characters'),
+                                                          EqualTo('password_hash2', message='passwords must match')
+                                                          ])
     password_hash2 = PasswordField("Confirm Password", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+    def validate_password_hash(self, password_hash):
+        if not bool(safe.check(password_hash.data)):
+            print('password insecure')
+            raise ValidationError('password not strong enough: include upper and lowercase, '
+                                  'special characters and numbers!!')
 
-# wtf forms > can also use for reCaptcha etc.
+
+
 class NamerForm(FlaskForm):
     name = StringField("Enter Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
@@ -23,4 +39,5 @@ class NamerForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
+    recaptcha = RecaptchaField()
     submit = SubmitField("Submit")
